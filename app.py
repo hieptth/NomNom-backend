db5e09bdbca501455eafdc8019e410ec6f3758b6
Khang Manh Nguyen <nguyenmanhkhang0805@gmail.com>
2024-03-29 08:18:07 +0700

    feat(db): food, tag, and food_tag tables

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from supabase import create_client, Client
+import os
+
+from flask import Flask, request
+
 
 app = Flask(__name__)
-
-
+SUPABASE_PROJECT_URL: str = os.getenv("SUPABASE_PROJECT_URL")
+SUPABASE_API_KEY: str = os.getenv("SUPABASE_API_KEY")
+supabase: Client = create_client(
+    SUPABASE_API_KEY,
+    SUPABASE_API_KEY
+)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello bruuuuuu'
