39501a1a4eac843ea8e9badcc809356f4509b54b
Khang Manh Nguyen <nguyenmanhkhang0805@gmail.com>
2024-04-02 01:07:48 +0700

    feat: get food by ID

--- app.py
+++ app.py
@@ -1,20 +1,35 @@
-from flask import Flask
 from supabase import create_client, Client
 import os
 
 from flask import Flask, request
 
-
 app = Flask(__name__)
-SUPABASE_PROJECT_URL: str = os.getenv("SUPABASE_PROJECT_URL")
-SUPABASE_API_KEY: str = os.getenv("SUPABASE_API_KEY")
+try:
+    SUPABASE_PROJECT_URL: str = os.getenv("SUPABASE_PROJECT_URL")
+    SUPABASE_API_KEY: str = os.getenv("SUPABASE_API_KEY")
+except None:
+    SUPABASE_PROJECT_URL: str = "default"
+    SUPABASE_API_KEY: str = "default"
+
 supabase: Client = create_client(
-    SUPABASE_API_KEY,
+    SUPABASE_PROJECT_URL,
     SUPABASE_API_KEY
 )
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello bruuuuuu'
+
+
+@app.route('/foods/<food_id>', methods=["GET"])
+def get_food(food_id:int):
+    print(food_id)
+    data, _ = supabase.table('food').select('*').eq('food_id', food_id).execute()
+    print(f"Data {data}")
+    return data[1][0]
+    # 1 for the actual json list is in the second object
+    # 0 for the json is the only element in json list
 
 
 if __name__ == '__main__':
